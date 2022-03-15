ea5c2b6090ba7275456ab166c5967a5fa5691008
IamRystam <us198211@rambler.ru>
2022-03-15 21:25:54 +0500

    Second

--- src/main/kotlin/Kotlin task 2-2.kt
+++ src/main/kotlin/Kotlin task 2-2.kt
@@ -1,7 +1,7 @@
 fun main() {
     println("Ведите количество лайков")
     val likes = readLine()?.toInt() ?: return
-    val person = if ((likes % 10 == 1) && (likes != 11)) "человеку"
+    val person = if ((likes % 10 == 1) && (likes % 100 != 11)) "человеку"
     else "людям"
     println("Понравилось $likes $person")
 
